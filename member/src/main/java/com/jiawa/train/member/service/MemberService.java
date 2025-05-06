e4a7abf751d3b3188b422ca0459551d6ad0f9892
Quafather <3224606259@qq.com>
2025-05-06 19:55:28 +0800

    简单的注册接口

--- member/src/main/java/com/jiawa/train/member/service/MemberService.java
+++ member/src/main/java/com/jiawa/train/member/service/MemberService.java
@@ -1,8 +1,13 @@
 package com.jiawa.train.member.service;
 
+import cn.hutool.core.collection.CollUtil;
+import com.jiawa.train.member.domain.Member;
+import com.jiawa.train.member.domain.MemberExample;
 import com.jiawa.train.member.mapper.MemberMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class MemberService {
@@ -10,5 +15,22 @@ public class MemberService {
     private MemberMapper memberMapper;
     public int count(){
         return (int) memberMapper.countByExample(null);
+    }
+
+    public long register(String mobile){
+        MemberExample memberExample = new MemberExample();
+        //createCriteria()是创建一个while条件
+        memberExample.createCriteria().andMobileEqualTo(mobile);
+        List<Member> list = memberMapper.selectByExample(memberExample);
+        if(CollUtil.isNotEmpty(list)){
+            //return list.get(0).getId();
+            throw new RuntimeException("the mobile phone has been created!");
+        }
+
+        Member member = new Member();//数据库的表中的一条记录结构
+        member.setId(System.currentTimeMillis());//用系统的时间作为id
+        member.setMobile(mobile);
+        memberMapper.insert(member);
+        return member.getId();
     }
 }
